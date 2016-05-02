d10006c46414c566d94adf33b9d79dda19641b1f
Javier Arboleda <javier.g.arboleda@gmail.com>
2016-05-01 22:32:57 -0700

    feat: Add animation to crop
    Added an animation that will crop image from bottom. Next step is to
    combine the animation that moves and zooms with cropping animation.

--- app/src/main/java/com/javierarboleda/projectezio/ComicActivity.java
+++ app/src/main/java/com/javierarboleda/projectezio/ComicActivity.java
@@ -1,13 +1,15 @@
 package com.javierarboleda.projectezio;
 
+import android.animation.ValueAnimator;
 import android.graphics.PointF;
+import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Toast;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
@@ -16,6 +18,7 @@ import com.javierarboleda.projectezio.comic.FileService;
 public class ComicActivity extends AppCompatActivity {
 
     SubsamplingScaleImageView mImageView;
+    TextView mCoordTextView;
     float mX;
     float mY;
 
@@ -23,6 +26,8 @@ public class ComicActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comic);
+
+        mCoordTextView = (TextView) findViewById(R.id.text);
 
         testLoadImage();
     }
@@ -38,6 +43,10 @@ public class ComicActivity extends AppCompatActivity {
 
         // imageView.startAnimation(someAnimation);
 
+        mImageView.setPanLimit(SubsamplingScaleImageView.PAN_LIMIT_OUTSIDE);
+
+
+
         mImageView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -45,7 +54,7 @@ public class ComicActivity extends AppCompatActivity {
                 mX = event.getX();
                 mY = event.getY();
 
-                // todo: add an overlaying TextView showing coordinates
+                mCoordTextView.setText("coordinates: " + mX + ", " + mY);
 
                 return false;
             }
@@ -55,15 +64,44 @@ public class ComicActivity extends AppCompatActivity {
             @Override
             public boolean onLongClick(View v) {
 
+                /**
+                   Using techniques below, going to try to move, zoom in or out, and crop all at
+                   once.
+
+                   Second animation cancels first, so comment one animation out to see the other.
+
+                   I think next step is to research and test animations out more, see if there
+                   is a way to combine what I'm looking for in one animation, i.e. move, zoom, crop.
+                */
+
+                // creates an animation that moves and zooms
+
                 mImageView.animateScaleAndCenter(1.5f, new PointF(mX + mX/2, mY + mY/2))
                         .withDuration(2000)
                         .withEasing(SubsamplingScaleImageView.EASE_OUT_QUAD)
                         .withInterruptible(false)
                         .start();
 
+                // creates an animation which crops from bottom up
+
+                ValueAnimator anim = ValueAnimator.ofInt(mImageView.getMeasuredHeight(), 0);
+                anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                        int val = (Integer) valueAnimator.getAnimatedValue();
+                        ViewGroup.LayoutParams layoutParams = mImageView.getLayoutParams();
+                        layoutParams.height = val;
+                        mImageView.setLayoutParams(layoutParams);
+                    }
+                });
+                anim.setDuration(1000);
+                anim.start();
+
                 return false;
             }
         });
+
+
 
 
     }
